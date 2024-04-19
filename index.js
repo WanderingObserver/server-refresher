a874777a4a965ee00572bc52e2ffc61941cd733d
Mohammed Chisti <93mchisti@gmail.com>
2024-04-19 15:48:30 -0400

    I should have branched this.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+require('./routes/authRoutes')(app)
+require('./services/passport')
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' })
