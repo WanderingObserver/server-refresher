de42fc337dadcd572c4d720a6e905d67ba663a47
Mohammed Chisti <93mchisti@gmail.com>
2024-04-19 18:46:09 -0400

    Added user verification for DB. Set methods for cookie session setup.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 require('./routes/authRoutes')(app)
-require('./services/passport')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+require('./models/User')
+require('./services/passport')
 const keys = require('./config/keys')
 
 mongoose.connect(keys.mongoURI)
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
+  res.send({ 
+    hi: "You're probably getting hacked right now ;). JK!~ Or am I? Sleep with one eye open. Unless you're one eye'd Jack. Jack-o-lantern. Carve ye another eye and a heart. Tear my heart open just to feel.",
+    papa_roach: "Drunk and I'm feeling down. And I just want to be alone.",
+    sora: "I've been having these weird thoughts lately. Like is any of this real or not?", 
+    hikaru_utada: "When we are older you'll understand. What I meant when I said, 'いいえ、人生はそれほど単純ではないと思います.'"
+  })
 })
 
 const PORT = process.env.PORT || 5001
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
-const passport = require('passport')
+const mongoose = require('mongoose')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const passport = require('passport')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+})
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
@@ -10,4 +22,15 @@ passport.use(new GoogleStrategy({
   console.log(accessToken)
   console.log('Refresh Token', refreshToken)
   console.log('Profile', profile)
+
+  User.findOne({ googleId: profile.id })
+    .then(existingUser => {
+      if(existingUser) {
+        done(null, existingUser)
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user))
+      }
+    })
 }))
