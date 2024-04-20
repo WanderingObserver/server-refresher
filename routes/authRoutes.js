db980f08eaea7e8cdb1a8e358c0799693a1c6e09
Mohammed Chisti <93mchisti@gmail.com>
2024-04-19 23:38:59 -0400

    clean up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,13 @@ module.exports = app => {
     app.get('/api/current_user', (req, res) => {
         res.send(req.user)
     })
+
+    app.get('/api/logout', (req, res) => {
+        // Watch video 49.
+        req.logout()
+        res.send({ 
+            user: "Logged out", 
+            response: req.user 
+        })
+    })
 }
